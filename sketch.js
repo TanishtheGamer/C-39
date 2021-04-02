a7fad2e1431cd6216b92ef679695c4bc98445cc5
TanishtheGamer <tanishrreddy@gmail.com>
2021-04-01 17:46:44 -0700

    animation

--- sketch.js
+++ sketch.js
@@ -6,9 +6,23 @@ var allPlayers;
 var distance = 0;
 var database;
 
+var car1_img, car2_img, car3_img, car4_img
+
+var track_img
+
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+
+function preload(){
+  car1_img = loadImage("../images/car1.png")
+  car2_img = loadImage("../images/car2.png")
+  car3_img = loadImage("../images/car3.png")
+  car4_img = loadImage("../images/car4.png")
+  track_img = loadImage("../images/track.jpg")
+
+  
+}
 
 
 function setup(){
